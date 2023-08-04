2e63cde43e002737c9ef491427681247cd5a3f15
Vladimir Jimenez <allejo@me.com>
2023-08-04 15:48:15 -0700

    Initial commit + version of @allejo/prettier-config

---
+++ packages/prettier-config/README.md
@@ -0,0 +1,17 @@
+# @allejo/prettier-config
+
+## Usage
+
+With npm v7+, peer dependencies will automatically be installed. However, if you're using an older version npm, you will need to install the peer dependencies of this package manually.
+
+```bash
+npm install --save-dev @allejo/prettier-config
+```
+
+```js
+// package.json
+{
+    // ...
+    "prettier": "@allejo/prettier-config"
+}
+```
---
+++ packages/prettier-config/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+	tabWidth: 2,
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	semi: true,
+	importOrder: [
+		'^(?!\\.+\\/).*(?<!\\.(s?css))$',
+		'^[./]+[^.]+$',
+		'^\\w.*\\.[a-z]+$',
+		'\\.[a-z]+$',
+	],
+	importOrderSeparation: true,
+	importOrderParserPlugins: ['jsx', 'typescript'],
+	plugins: [
+		'@trivago/prettier-plugin-sort-imports',
+		'prettier-plugin-packagejson',
+	],
+};
