1bf48463612d22e85dc51f066981e8b25a59c20e
Vladimir Jimenez <allejo@me.com>
2023-08-04 15:52:38 -0700

    Fix formatting of index.js

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -4,7 +4,7 @@ module.exports = {
 	singleQuote: true,
 	trailingComma: 'all',
 	semi: true,
-  endOfLine: 'auto',
+	endOfLine: 'auto',
 	importOrder: [
 		'^(?!\\.+\\/).*(?<!\\.(s?css))$',
 		'^[./]+[^.]+$',
